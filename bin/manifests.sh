d8cd0a588e8fcc8e123833906c4c1b6dec3c8a19
Ryan Geyer <ryan.geyer@turbonomic.com>
2020-06-25 23:36:26 +0000

    Jenkins has no TTY

--- bin/manifests.sh
+++ bin/manifests.sh
@@ -2,7 +2,7 @@
 
 # Provided the suffix of a manifest file, this will return which version component
 # if any needs to be incremented for semantic versioning
-# 
+#
 # Return values are 3==Major 2==Minor 1==Patch 0==No Change
 determine_increment () {
   manifest_suffix=$1
@@ -26,7 +26,7 @@ determine_increment () {
   then
     return 1
   fi
-  
+
   return 0
 }
 
@@ -46,19 +46,19 @@ else
 fi
 
 docker inspect --format='{{index .RepoDigests 0}}' python:3.8-alpine > manifest.alpine
-docker run --rm -it turbointegrations/base:alpine-build sh -c "python --version" >> manifest.alpine
-docker run --rm -it turbointegrations/base:alpine-build sh -c "pip -V" >> manifest.alpine
-docker run --rm -it turbointegrations/base:alpine-build sh -c "pip freeze" >> manifest.alpine
+docker run --rm -i turbointegrations/base:alpine-build sh -c "python --version" >> manifest.alpine
+docker run --rm -i turbointegrations/base:alpine-build sh -c "pip -V" >> manifest.alpine
+docker run --rm -i turbointegrations/base:alpine-build sh -c "pip freeze" >> manifest.alpine
 
 docker inspect --format='{{index .RepoDigests 0}}' python:3.8-slim-buster > manifest.slim-buster
-docker run --rm -it turbointegrations/base:slim-buster-build sh -c "python --version" >> manifest.slim-buster
-docker run --rm -it turbointegrations/base:slim-buster-build sh -c "pip -V" >> manifest.slim-buster
-docker run --rm -it turbointegrations/base:slim-buster-build sh -c "pip freeze" >> manifest.slim-buster
+docker run --rm -i turbointegrations/base:slim-buster-build sh -c "python --version" >> manifest.slim-buster
+docker run --rm -i turbointegrations/base:slim-buster-build sh -c "pip -V" >> manifest.slim-buster
+docker run --rm -i turbointegrations/base:slim-buster-build sh -c "pip freeze" >> manifest.slim-buster
 
 docker inspect --format='{{index .RepoDigests 0}}' registry.access.redhat.com/ubi8/python-38 > manifest.rhel
-docker run --rm -it turbointegrations/base:rhel-build sh -c "python --version" >> manifest.rhel
-docker run --rm -it turbointegrations/base:rhel-build sh -c "pip -V" >> manifest.rhel
-docker run --rm -it turbointegrations/base:rhel-build sh -c "pip freeze" >> manifest.rhel
+docker run --rm -i turbointegrations/base:rhel-build sh -c "python --version" >> manifest.rhel
+docker run --rm -i turbointegrations/base:rhel-build sh -c "pip -V" >> manifest.rhel
+docker run --rm -i turbointegrations/base:rhel-build sh -c "pip freeze" >> manifest.rhel
 
 if [[ "$FIRST_RUN" -ne "0" ]]
 then
@@ -86,7 +86,7 @@ then
       MINOR=0
       PATCH=0
       echo "Major version increment detected. Manual intervention required"
-      exit 1 
+      exit 1
       ;;
     2)
       MINOR=$((MINOR+1))
